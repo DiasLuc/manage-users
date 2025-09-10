23835a8f943a12597085775dcec2694c9b7ed239
DiasLuc <diaslucp@gmail.com>
2025-09-09 21:56:37 -0400

    Changed list of user names so they become objects

--- src/manageUsers.js
+++ src/manageUsers.js
@@ -1,11 +1,28 @@
-const users = ['Matthew', 'Mark', 'Luke', 'John'];
+const users = [
+                {
+                    name: 'Matthew',
+                    email: 'matthew@disciples.org'
+                },
+                {
+                    name: 'Mark',
+                    email: 'mark@disciples.org'
+                }, 
+                {
+                    name: 'Luke',
+                    email: 'luke@disciples.org'
+                }, 
+                {
+                    name: 'John',
+                    email: 'john@disciples.org'
+                }
+            ];
 
 function returnUsers() {
     return users;
 };
 
-function addNewUser(name) {
-    users.push(name);
+function addNewUser(user) {
+    users.push(user);
 };
 
 module.exports = {
--- test/manageUsers.test.js
+++ test/manageUsers.test.js
@@ -3,8 +3,11 @@ const { expect } = require('chai');
 
 describe('Test the functions for Managing Users', ()=>{
     it('Validates that I\'m able to add a new name to the user list', () => {
-        addNewUser('Judas');
+        addNewUser({name: 'Judas', email:'judas@disciples.org'});
         const userList = returnUsers();
-        expect(userList.at(-1)).to.equal('Judas');
+        expect(userList.at(-1).name).to.equal('Judas');
+        expect(userList.at(-1).email).to.equal('judas@disciples.org');
+        // .to.eql works in the same way as .to.deep.equal; used to compare objects
+        expect(userList.at(-1)).to.eql({name: 'Judas', email:'judas@disciples.org'});
     })
 })
